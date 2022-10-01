ab3f0ab086726b30925971f623bb45e117ee9d18
HappyFacell <is722820@iteso.mx>
2022-10-01 13:44:27 -0500

    Tuning

--- practica1/lib/pages/favorite/fav_section.dart
+++ practica1/lib/pages/favorite/fav_section.dart
@@ -11,6 +11,7 @@ class FavSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xff042442),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
