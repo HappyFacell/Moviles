b05fc64ae3ccfed28f53dd7b90d26bca77de847a
HappyFacell <is722820@iteso.mx>
2022-10-18 07:53:09 -0500

    Foto Share

---
+++ foto_share/lib/pages/espera/en_espera.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EnEspera extends StatelessWidget {
+  const EnEspera({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
