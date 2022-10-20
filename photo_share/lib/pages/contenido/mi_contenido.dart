2236d4e015be5d88c80caee6f3c1b2b58eee4c01
HappyFacell <is722820@iteso.mx>
2022-10-20 09:53:22 -0500

    Aqui vamos de nuevo

--- photo_share/lib/pages/contenido/mi_contenido.dart
+++ photo_share/lib/pages/contenido/mi_contenido.dart
@@ -5,6 +5,8 @@ class MiContenido extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Center(
+      child: Text("Mi contenido"),
+    );
   }
 }
