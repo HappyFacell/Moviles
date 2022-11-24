d130e814ddf5cabc4d443a5808b2d276942fd747
HappyFacell <is722820@iteso.mx>
2022-11-24 17:50:52 -0600

    Falto algo

--- practica22/lib/pages/favorite/favorite_page.dart
+++ practica22/lib/pages/favorite/favorite_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 import 'bloc/favorite_bloc.dart';
 
@@ -102,7 +103,7 @@ class _FavoritePageState extends State<FavoritePage> {
                         child: const Text("Yes"),
                         onPressed: () {
                           Navigator.pop(context);
-                          _launchUrl('${favourites[index]['listenLink']}');
+                          launchUrlString('${favourites[index]['listenLink']}',mode: LaunchMode.externalApplication);
                         },
                       ),
                       TextButton(
